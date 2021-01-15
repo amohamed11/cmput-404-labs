346cf2729da424f576f2bbd9fbb17e0f3874c5b3
Anas Mohamed <amohamed@ualberta.ca>
2021-01-14 18:47:24 -0700

    updated script url

--- lab1/script.py
+++ lab1/script.py
@@ -4,9 +4,9 @@ print("requests version:")
 print(requests.__version__)
 
 r = requests.get("https://www.google.com")
-print("google:")
+print("\ngoogle:")
 print(r.text)
 
-r = requests.get("https://raw.githubusercontent.com/amohamed11/cmput-404-labs/lab1/script.py")
-print("github script:")
+r = requests.get("https://raw.githubusercontent.com/amohamed11/cmput-404-labs/main/lab1/script.py")
+print("\ngithub script:")
 print(r.text)
